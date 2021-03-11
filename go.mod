b3283494b937cc158e905b16e6ee5b70dd6e78b0
Anjan Roy <anjanroy@yandex.com>
2021-03-11 12:58:34 +0530

    init setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/itzmeanjan/gasbot
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+func main() {
+
+	log.Printf("🚀 gasbot - Telegram Bot for Ethereum Gas Price Notification")
+
+}
