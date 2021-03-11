1ef4a73a7afb57aad15b43524c8cd3f4a71a6605
Anjan Roy <anjanroy@yandex.com>
2021-03-11 13:33:28 +0530

    returns full URL for querying current gas price

--- app/config/read.go
+++ app/config/read.go
@@ -19,3 +19,15 @@ func Get(key string) string {
 func GetUint(key string) uint {
 	return viper.GetUint(key)
 }
+
+// GetGaszQueryURL - Returns full url of `gasz - Ethereum Gas Price Notifier`
+// where GET request can be sent, for getting latest gas price recommendation
+func GetGaszQueryURL() string {
+
+	if url := Get("GASZ"); url != "" {
+		return url
+	}
+
+	return "https://gasz.in/v1/latest"
+
+}
