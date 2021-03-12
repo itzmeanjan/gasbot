8eb63005b2cfe31f5545ec92be95ca060d96aaeb
Anjan Roy <anjanroy@yandex.com>
2021-03-12 15:40:08 +0530

    subscription, unsubscription confirmation message

--- app/data/data.go
+++ app/data/data.go
@@ -32,3 +32,10 @@ type Payload struct {
 	Threshold float64 `json:"threshold"`
 	Operator  string  `json:"operator"`
 }
+
+// Response - Subscription/ unsubscription confirmation messages
+// to be received in this form
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
